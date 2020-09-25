9ef72780645bd34bdb889e0f0030c0a84a04d2f8
rafaleal <rafaelalealmartinss@gmail.com>
2020-09-25 12:47:02 -0300

    currency converter

--- lib/main.dart
+++ lib/main.dart
@@ -7,7 +7,13 @@ const request = 'https://api.hgbrasil.com/finance?format=json-cors&key=4b9a52b0'
 
 void main() async {
   runApp(MaterialApp(
-    home: Home()
+    home: Home(),
+    theme: ThemeData(
+      brightness: Brightness.light,
+      primaryColor: Colors.amberAccent,
+      accentColor: Colors.amberAccent,
+      hintColor: Colors.amberAccent
+    )
   ));
 }
 
@@ -17,6 +23,34 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  final realController = TextEditingController();
+  final dolarController = TextEditingController();
+  final euroController = TextEditingController();
+
+  double dolar;
+  double euro;
+
+  void _realChanged(String text) {
+    double real = double.parse(text);
+
+    dolarController.text = (real/dolar).toStringAsFixed(2);
+    euroController.text = (real/euro).toStringAsFixed(2);
+  }
+
+  void _dolarChanged(String text) {
+    double dolar = double.parse(text);
+
+    realController.text = (dolar * this.dolar).toStringAsFixed(2);
+    euroController.text = (dolar * this.dolar / euro).toStringAsFixed(2);
+  }
+
+  void _euroChanged(String text) {
+    double euro = double.parse(text);
+
+    realController.text = (euro * this.euro).toStringAsFixed(2);
+    euroController.text = (euro * this.euro / dolar).toStringAsFixed(2);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -25,9 +59,72 @@ class _HomeState extends State<Home> {
         title: Text("Conversor de Moedas"),
         backgroundColor: Colors.amberAccent,
         centerTitle: true,
-      )
+      ),
+      body: FutureBuilder<Map>(
+        future: getData(),
+        builder: (context, snapshot) {
+          switch(snapshot.connectionState){
+            case ConnectionState.none:
+            case ConnectionState.waiting:
+              return Center(
+                child: Text("Loading...",
+                  style: TextStyle(
+                    color:Colors.amberAccent,
+                    fontSize: 25.0
+                  ),
+                ),
+              );
+            default:
+              if(snapshot.hasError) {
+                return Center(
+                  child: Text("Sorry, try again.",
+                    style: TextStyle(
+                      color: Colors.redAccent,
+                      fontSize: 25.0),
+                    textAlign: TextAlign.center
+                  )
+                );
+              } else {
+                dolar = snapshot.data["results"]["currencies"]["USD"]["buy"];
+                euro = snapshot.data["results"]["currencies"]["EUR"]["buy"];
+
+                return SingleChildScrollView(
+                  padding: EdgeInsets.all(10.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: <Widget>[
+                      Icon(Icons.monetization_on, size: 100.0, color: Colors.amberAccent),
+                      buildTextField("Reais", "R\$", realController, _realChanged),
+                      Divider(),
+                      buildTextField("Dólares", "\$", dolarController, _dolarChanged),
+                      Divider(),
+                      buildTextField("Euros", "€", euroController, _euroChanged)
+                    ],
+                  ),
+                );
+              }
+          }
+        },
+      ),
     );
   }
+}
+
+Widget buildTextField(String label, String prefix, TextEditingController textController, Function textFunction) {
+  return TextField(
+    controller: textController,
+    decoration: InputDecoration(
+        labelText: label,
+        labelStyle: TextStyle(color: Colors.amberAccent),
+        border: OutlineInputBorder(),
+        prefixText: prefix
+    ),
+    style: TextStyle(
+        color: Colors.amberAccent, fontSize: 22.0
+    ),
+    onChanged: textFunction,
+    keyboardType: TextInputType.number,
+  );
 }
 
 
