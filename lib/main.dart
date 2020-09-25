e8505036f6607bcaf6bd455dbb7cbdf759c1b159
rafaleal <rafaelalealmartinss@gmail.com>
2020-09-25 11:43:57 -0300

    Scaffold with appBar

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,38 @@
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
-import 'package:async/async.dart';
+import 'dart:async';
+import 'dart:convert';
 
 const request = 'https://api.hgbrasil.com/finance?format=json-cors&key=4b9a52b0';
 
 void main() async {
+  runApp(MaterialApp(
+    home: Home()
+  ));
+}
 
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        title: Text("Conversor de Moedas"),
+        backgroundColor: Colors.amberAccent,
+        centerTitle: true,
+      )
+    );
+  }
+}
+
+
+Future<Map> getData() async {
   http.Response response = await http.get(request);
 
-  runApp(MaterialApp(
-    home: Container()
-  ));
+  return json.decode(response.body);
 }
